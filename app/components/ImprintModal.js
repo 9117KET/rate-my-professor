d8b80562b96fd481e51daa6986a8f8c5ec802be3
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2025-02-14 16:45:03 +0100

    update the contact

--- app/components/ImprintModal.js
+++ app/components/ImprintModal.js
@@ -29,7 +29,7 @@ export const ImprintModal = ({ open, onClose }) => {
           <Typography variant="h6" sx={{ mb: 2 }}>
             Service Provider
           </Typography>
-          <Typography>Kinlo Ephriam Tangiri | Personal Project</Typography>
+          <Typography>Kinlo ET | Personal Fun Project</Typography>
           <Typography>Campus Ring 1</Typography>
           <Typography>28759 Bremen</Typography>
           <Typography>Germany</Typography>
@@ -39,8 +39,16 @@ export const ImprintModal = ({ open, onClose }) => {
           <Typography variant="h6" sx={{ mb: 2 }}>
             Contact
           </Typography>
-          <Typography>Email: kephriamtangiri@constructor.university</Typography>
-          <Typography>Phone: +49 17627522943</Typography>
+          <Typography>
+            <a
+              href="https://www.linkedin.com/in/kinlo-ephriam-tangiri-a70113218/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "#0077b5", textDecoration: "none" }}
+            >
+              LinkedIn
+            </a>
+          </Typography>
         </Box>
 
         <Box sx={{ mb: 4 }}>
@@ -49,8 +57,8 @@ export const ImprintModal = ({ open, onClose }) => {
           </Typography>
           <Typography paragraph>
             This platform is for informational purposes only. The reviews and
-            ratings are user-generated content and do not represent my official views or the
-            views of Constructor University.
+            ratings are user-generated content and do not represent my official
+            views or the views of Constructor University.
           </Typography>
           <Typography paragraph>
             We strive to maintain the accuracy of information but make no
@@ -68,9 +76,11 @@ export const ImprintModal = ({ open, onClose }) => {
             Data Protection
           </Typography>
           <Typography paragraph>
-            Data generated on this platform is anonymous data and we have no way to track the user data for the purpose of giving genuent review of their experiences with professor at Constructor University
-            I strive to make sure the data is in accordance with the EU General Data
-            Protection Regulation (GDPR). 
+            Data generated on this platform is anonymous data and we have no way
+            to track the user data for the purpose of giving genuent review of
+            their experiences with professor at Constructor University. I strive
+            to make sure the data is in accordance with the EU General Data
+            Protection Regulation (GDPR).
           </Typography>
         </Box>
       </DialogContent>
