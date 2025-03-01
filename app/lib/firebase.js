7efbe2fa3ca0c56230cf8c45ea6073f4b3f2bce9
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2025-03-01 21:48:21 +0100

    fix deployment issue

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -8,13 +8,24 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDSEjW_KrlgT8qHEoaeFYlNbqgvyVETGyY",
-  authDomain: "ratemyprofessor-99fb5.firebaseapp.com",
-  projectId: "ratemyprofessor-99fb5",
-  storageBucket: "ratemyprofessor-99fb5.firebasestorage.app",
-  messagingSenderId: "177122975941",
-  appId: "1:177122975941:web:17578a39295416155150ac",
-  measurementId: "G-HMGH83WRN5",
+  apiKey:
+    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
+    "AIzaSyDSEjW_KrlgT8qHEoaeFYlNbqgvyVETGyY",
+  authDomain:
+    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
+    "ratemyprofessor-99fb5.firebaseapp.com",
+  projectId:
+    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "ratemyprofessor-99fb5",
+  storageBucket:
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
+    "ratemyprofessor-99fb5.firebasestorage.app",
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "177122975941",
+  appId:
+    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
+    "1:177122975941:web:17578a39295416155150ac",
+  measurementId:
+    process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-HMGH83WRN5",
 };
 
 // Initialize Firebase
